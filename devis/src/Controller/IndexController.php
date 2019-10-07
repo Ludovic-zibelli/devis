1fc6af348c68451465bd363db7076ca12a46fcda
ludovic.zibelli <ludovic.zibelli@free.fr>
2019-10-07 22:24:54 +0200

    Mise en forme + debut gestion des donnés en base de données

--- devis/src/Controller/IndexController.php
+++ devis/src/Controller/IndexController.php
@@ -3,15 +3,38 @@
 namespace App\Controller;
 
 use App\Form\EstimationType;
+use App\Repository\EstimationRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IndexController extends AbstractController
 {
     /**
-     * @Route("/index/home")
+     * @var EstimationRepository
      */
-    public function home()
+    private $repository;
+
+    private $em;
+
+    /**
+     * IndexController constructor.
+     * @param EstimationRepository $repository
+     * @param ObjectManager $em
+     */
+    public function __construct(EstimationRepository $repository, ObjectManager $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/" , name="home")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function home(Request $request)
     {
         $form = $this->createForm(EstimationType::class);
 
