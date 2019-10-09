eb82ab88b48e1ee4d4768be9c6ab551e0a8bc8d5
kuma-supesu <jkenobi@free.fr>
2019-10-09 18:10:36 +0200

    suite authentification

--- estimation/src/Controller/CrmController.php
+++ estimation/src/Controller/CrmController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -8,6 +9,8 @@ class CrmController extends AbstractController
 {
     /**
      * @Route("/crm", name="crm")
+     *
+     *  @Security("is_granted('ROLE_ADMIN')")
      */
     public function crmAction()
     {
